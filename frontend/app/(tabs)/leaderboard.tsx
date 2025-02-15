7f005e11ae5cae4b343094cf8c37b304e07917c1
Zhuo En <chuazhuoen@gmail.com>
2025-02-16 00:47:09 +0800

    leaderboard

--- frontend/app/(tabs)/leaderboard.tsx
+++ frontend/app/(tabs)/leaderboard.tsx
@@ -1,9 +1,41 @@
-import { StyleSheet, Image, Platform, View, Alert, Text } from 'react-native';
-import { ButtonGroup } from 'react-native-elements';
+import { BackendUrl } from '@/context/backendUrl';
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 import Leaderboard from 'react-native-leaderboard';
 
-export default function leaderboard() {
-    return <>
-    
-    </>
+const LData = [
+    { userName: 'Joe', highScore: 52 },
+    { userName: 'Jenny', highScore: 120 },
+];
+
+export default function LeaderboardComponent() {
+    return (
+        <View style={styles.container}>
+            <Leaderboard
+                data={fetchPosts}
+                sortBy='highScore'
+                labelBy='userName'
+            />
+        </View>
+    );
 }
+
+const fetchPosts = async () => {
+    try {
+        const response = await fetch(`${BackendUrl}/feed`);
+        const json = await response.json();
+        return json.posts || [];
+    } catch (error) {
+        console.error('Error fetching posts', error);
+        return [];
+    }
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    },
+});
